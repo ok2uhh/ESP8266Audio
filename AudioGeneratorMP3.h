250bc24f896f484fcc71ff982bc68d23c84a83ac
ok2uhh <ok2uhh@gmail.com>
2025-09-19 09:41:03 +0200

    Create AudioGeneratorMP3.h

---
+++ AudioGeneratorMP3.h
@@ -0,0 +1,17 @@
+#ifndef _AUDIOGENERATORMP3_H
+#define _AUDIOGENERATORMP3_H
+
+#include "AudioFileSource.h"
+#include "AudioOutput.h"
+#include "AudioGenerator.h"
+
+class AudioGeneratorMP3 : public AudioGenerator {
+  public:
+    AudioGeneratorMP3();
+    virtual ~AudioGeneratorMP3();
+    virtual bool begin(AudioFileSource &input, AudioOutput &output);
+    virtual bool loop();
+    virtual bool stop();
+};
+
+#endif
